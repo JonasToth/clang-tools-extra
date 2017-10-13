16833f3be1e10448381fa65cc301a195e176a2ce
Jonas Toth <jonas.toth@gmail.com>
2017-10-13 15:44:12 +0200

    work on testing code and matchers

--- clang-tidy/cppcoreguidelines/MixedIntArithmeticCheck.cpp
+++ clang-tidy/cppcoreguidelines/MixedIntArithmeticCheck.cpp
@@ -11,6 +11,8 @@
 #include "clang/AST/ASTContext.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
+#include <iostream>
+
 using namespace clang::ast_matchers;
 
 namespace clang {
@@ -20,28 +22,51 @@ namespace cppcoreguidelines {
 void MixedIntArithmeticCheck::registerMatchers(MatchFinder *Finder) {
   const auto UnsignedIntegerOperand =
       expr(ignoringImpCasts(hasType(isUnsignedInteger())))
-          .bind("unsigned-operand");
+          .bind("unsigned-binary-operand");
+  const auto SignedIntegerOperand =
+      expr(ignoringImpCasts(hasType(isSignedInteger())))
+          .bind("signed-binary-operand");
 
   // Match binary bitwise operations on signed integer arguments.
   Finder->addMatcher(
       binaryOperator(allOf(anyOf(hasOperatorName("+"), hasOperatorName("-"),
                                  hasOperatorName("*"), hasOperatorName("/")),
                            hasEitherOperand(UnsignedIntegerOperand),
+                           hasEitherOperand(SignedIntegerOperand),
                            hasRHS(hasType(isInteger())),
                            hasLHS(hasType(isInteger()))))
-          .bind("mixed-arithmetic"),
+          .bind("mixed-binary-arithmetic"),
       this);
 }
 
 void MixedIntArithmeticCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *UnsignedOperand =
-      Result.Nodes.getNodeAs<Expr>("unsigned-operand");
+      Result.Nodes.getNodeAs<Expr>("unsigned-binary-operand");
+  //const auto *SignedOperand =
+      //Result.Nodes.getNodeAs<Expr>("signed-binary-operand");
   const auto *MixedArithmetic =
-      Result.Nodes.getNodeAs<BinaryOperator>("mixed-arithmetic");
+      Result.Nodes.getNodeAs<BinaryOperator>("mixed-binary-arithmetic");
 
-  diag(MixedArithmetic->getLocStart(), "use of signed and unsigned integers in "
-                                       "arithmetic expression; use signed "
-                                       "integers only")
+  assert(UnsignedOperand && MixedArithmetic &&
+         "Matcher did not match operand and operation");
+
+  diag(MixedArithmetic->getLocStart(),
+       "mixed signed and unsigned arithmetic; "
+       "prefer signed integers and use unsigned types only for modulo "
+       "arithmetic")
+      << MixedArithmetic->getSourceRange();
+
+  /*
+  std::cout << "Debug: signed expr = '"
+            << std::string(Lexer::getSourceText(
+                   CharSourceRange::getTokenRange(
+                       SignedOperand->getSourceRange()),
+                   *Result.SourceManager, getLangOpts(), 0))
+            << "'\n";
+  */
+  //diag(SignedOperand->getLocStart(), "signed operand", DiagnosticIDs::Note)
+      //<< SignedOperand->getSourceRange();
+  diag(UnsignedOperand->getLocStart(), "unsigned operand", DiagnosticIDs::Note)
       << UnsignedOperand->getSourceRange();
 }
 
