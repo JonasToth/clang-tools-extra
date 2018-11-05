0831acb67111b07fe337f10104a5afe95839df12
Jonas Toth <development@jonas-toth.eu>
2018-11-05 20:02:25 +0100

    [Feature] implement deduplication parser and test on real data

--- clang-tidy/tool/test_log_parser.py
+++ clang-tidy/tool/test_log_parser.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from utility import Diagnostic, DiagEssence, Deduplication
-from utility import ParseBBOutput
 import unittest
+from utility import Diagnostic, DiagEssence, Deduplication
+from utility import ParseClangTidyDiagnostics, _is_valid_diag_match
 
 
 class TestDiagnostics(unittest.TestCase):
@@ -11,17 +11,17 @@ class TestDiagnostics(unittest.TestCase):
 
     def test_construction(self):
         d = Diagnostic("/home/user/project/my_file.h", 24, 4,
-                        "warning: Do not do this thing [warning-category]")
+                       "warning: Do not do this thing [warning-category]")
         self.assertIsNotNone(d)
 
         de = DiagEssence("/home/user/project/my_file.h", 24, 4,
                          "warning: Do not do this thing [warning-category]",
-                         "      MyCodePiece();\n"\
-                         "      ^\n")
+                         "\n      MyCodePiece();"
+                         "\n      ^")
         self.assertIsNotNone(de)
-        
-        d.add_additional_line("      MyCodePiece();\n")
-        d.add_additional_line("      ^\n")
+
+        d.add_additional_line("      MyCodePiece();")
+        d.add_additional_line("      ^")
 
         self.assertEqual(d.get_essence().__hash__(), de.__hash__())
         self.assertEqual(d.get_essence(), de)
@@ -29,11 +29,11 @@ class TestDiagnostics(unittest.TestCase):
     def test_comparisons(self):
         de1 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         de2 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertEqual(de1.__hash__(), de2.__hash__())
         self.assertEqual(de1, de2)
@@ -41,21 +41,21 @@ class TestDiagnostics(unittest.TestCase):
         # Changing location information must influence the hash.
         de3 = DiagEssence("/home/user/project/my_file.h", 24, 5,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertNotEqual(de1.__hash__(), de3.__hash__())
         self.assertNotEqual(de1, de3)
 
         de4 = DiagEssence("/home/user/project/my_file.h", 1, 4,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertNotEqual(de1.__hash__(), de4.__hash__())
         self.assertNotEqual(de1, de4)
 
         de5 = DiagEssence("/home/user/project/another_file.h", 24, 4,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertNotEqual(de1.__hash__(), de5.__hash__())
         self.assertNotEqual(de1, de5)
@@ -64,14 +64,14 @@ class TestDiagnostics(unittest.TestCase):
         # Does *NOT* have an influence on the hash.
         de6 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Do not do this thing [other-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertEqual(de1.__hash__(), de6.__hash__())
         self.assertNotEqual(de1, de6)
 
         de7 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Different message [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertEqual(de1.__hash__(), de7.__hash__())
         self.assertNotEqual(de1, de7)
@@ -91,7 +91,7 @@ class TestDiagnostics(unittest.TestCase):
 
 class TestDeduplication(unittest.TestCase):
     """Test the `DiagEssence` based deduplication of diagnostic messages."""
-    
+
     def test_construction(self):
         self.assertIsNotNone(Deduplication())
 
@@ -100,7 +100,7 @@ class TestDeduplication(unittest.TestCase):
 
         de1 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Do not do this thing [warning-category]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertTrue(dedup.insert_and_query(de1))
         self.assertFalse(dedup.insert_and_query(de1))
@@ -108,7 +108,7 @@ class TestDeduplication(unittest.TestCase):
         # Create a diagnostic with the same hash that is not equal.
         de2 = DiagEssence("/home/user/project/my_file.h", 24, 4,
                           "warning: Do not do this thing [mess-with-equality]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertTrue(dedup.insert_and_query(de2))
         self.assertFalse(dedup.insert_and_query(de2))
@@ -116,96 +116,169 @@ class TestDeduplication(unittest.TestCase):
         # Create a diagnostic with a different hash and insert it.
         de3 = DiagEssence("/home/user/project/mess_with_hash.h", 24, 5,
                           "warning: Do not do this thing [mess-with-equality]",
-                          "      MyCodePiece();\n"\
+                          "      MyCodePiece();\n"
                           "      ^\n")
         self.assertTrue(dedup.insert_and_query(de3))
         self.assertFalse(dedup.insert_and_query(de3))
 
-    
+
 class TestLinewiseParsing(unittest.TestCase):
-    @unittest.skip
-    def test_performance_output(self):
-        enabled_checks = [
-            "performance-faster-string-find",
-            "performance-for-range-copy",
-            "performance-implicit-conversion-in-loop",
-            "performance-inefficient-algorithm",
-            "performance-inefficient-string-concatenation",
-            "performance-inefficient-vector-operation",
-            "performance-move-const-arg",
-            "performance-move-constructor-init",
-            "performance-noexcept-move-constructor",
-            "performance-type-promotion-in-math-fn",
-            "performance-unnecessary-copy-initialization",
-            "performance-unnecessary-value-param",
+    def test_construction(self):
+        self.assertIsNotNone(ParseClangTidyDiagnostics())
+
+    def test_valid_diags_regex(self):
+        pp = ParseClangTidyDiagnostics()
+
+        warning = "/home/user/project/my_file.h:123:1: warning: don't do it [no]"
+        m = pp._diag_re.match(warning)
+        self.assertTrue(m)
+        self.assertTrue(_is_valid_diag_match(m.groups()))
+
+        error = "/home/user/project/my_file.h:1:110: error: wrong! [not-ok]"
+        m = pp._diag_re.match(error)
+        self.assertTrue(m)
+        self.assertTrue(_is_valid_diag_match(m.groups()))
+
+        hybrid = "/home/user/project/boo.cpp:30:42: error: wrong! [not-ok,bad]"
+        m = pp._diag_re.match(hybrid)
+        self.assertTrue(m)
+        self.assertTrue(_is_valid_diag_match(m.groups()))
+
+        note = "/home/user/project/my_file.h:1:110: note: alksdj"
+        m = pp._diag_re.match(note)
+        self.assertFalse(m)
+
+        garbage = "not a path:not_a_number:110: gibberish"
+        m = pp._diag_re.match(garbage)
+        self.assertFalse(m)
+
+    def test_single_diagnostics(self):
+        pp = ParseClangTidyDiagnostics()
+        example_warning = [
+            "/project/git/Source/kwsys/Terminal.c:53:21: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]",
         ]
-        performance_result = ParseBBOutput()
-        performance_result.parse_file("test_input/out_performance_cmake.log")
-
-        self.assertListEqual(performance_result.get_enabled_checks(),
-                             enabled_checks)
-
-    @unittest.skip
-    def test_hicpp_output(self):
-        enabled_checks = [
-            "hicpp-avoid-goto",
-            "hicpp-braces-around-statements",
-            "hicpp-deprecated-headers",
-            "hicpp-exception-baseclass",
-            "hicpp-explicit-conversions",
-            "hicpp-function-size",
-            "hicpp-invalid-access-moved",
-            "hicpp-member-init",
-            "hicpp-move-const-arg",
-            "hicpp-multiway-paths-covered",
-            "hicpp-named-parameter",
-            "hicpp-new-delete-operators",
-            "hicpp-no-array-decay",
-            "hicpp-no-assembler",
-            "hicpp-no-malloc",
-            "hicpp-noexcept-move",
-            "hicpp-signed-bitwise",
-            "hicpp-special-member-functions",
-            "hicpp-static-assert",
-            "hicpp-undelegated-constructor",
-            "hicpp-use-auto",
-            "hicpp-use-emplace",
-            "hicpp-use-equals-default",
-            "hicpp-use-equals-delete",
-            "hicpp-use-noexcept",
-            "hicpp-use-nullptr",
-            "hicpp-use-override",
-            "hicpp-vararg",
-        ]
-        hicpp_result = ParseBBOutput()
-        hicpp_result.parse_file("test_input/out_hicpp_cmake.log")
-
-        self.assertListEqual(hicpp_result.get_enabled_checks(), enabled_checks)
-
-        f = hicpp_result.get_findings()
-        n_warnings = 45407
-        self.assertEqual(f.total_finding_count(), n_warnings)
-
-        uniq_n_goto = 108
+        pp._parse_lines(example_warning)
         self.assertEqual(
-            f.unique_finding_count("hicpp-avoid-goto"), uniq_n_goto)
-        uniq_n_auto = 745
-        self.assertEqual(f.unique_finding_count("hicpp-use-auto"), uniq_n_auto)
-        all_n_auto = 878
-        self.assertEqual(f.total_finding_count("hicpp-use-auto"), all_n_auto)
+            str(pp.get_diags()[0]),
+            "/project/git/Source/kwsys/Terminal.c:53:21: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]"
+        )
 
-    def test_visual_inspection(self):
-        pass
-        # print(f.finding_checks())
-        # for diag in f.findings_for_check(self.result.get_enabled_checks()[0]):
-        # print(diag)
+    def test_no_diag(self):
+        pp = ParseClangTidyDiagnostics()
+        garbage_lines = \
+"""
+    hicpp-no-array-decay
+    hicpp-no-assembler
+    hicpp-no-malloc
+    hicpp-noexcept-move
+    hicpp-signed-bitwise
+    hicpp-special-member-functions
+    hicpp-static-assert
+    hicpp-undelegated-constructor
+    hicpp-use-auto
+    hicpp-use-emplace
+    hicpp-use-equals-default
+    hicpp-use-equals-delete
+    hicpp-use-noexcept
+    hicpp-use-nullptr
+    hicpp-use-override
+    hicpp-vararg
 
-        # for check in f.finding_checks():
-        # print("{}: {} unique findings".format(
-        # check, len(f.findings_for_check(check))))
+clang-apply-replacements version 8.0.0
+18 warnings generated.
+36 warnings generated.
+Suppressed 26 warnings (26 in non-user code).
+Use -header-filter=.* to display errors from all non-system headers. Use -system-headers to display errors from system headers as well.
 
-        # for (check, findings) in f.raw_results().items():
-        # print("{}:  {} raw findings".format(check, len(findings)))
+61 warnings generated.
+122 warnings generated.
+Suppressed 122 warnings (122 in non-user code).
+Use -header-filter=.* to display errors from all non-system headers. Use -system-headers to display errors from system headers as well.
+
+clang-tidy -header-filter=^/project/git/.* -checks=-*,hicpp-* -export-fixes /tmp/tmpH8MVt0/tmpErKPl_.yaml -p=/project/git /project/git/Source/kwsys/Terminal.c
+"""
+        pp.parse_string(garbage_lines)
+        self.assertEqual(len(pp.get_diags()), 0)
+
+    def test_deduplicate_basic_multi_line_warning(self):
+        pp = ParseClangTidyDiagnostics()
+        example_warning = [
+            "/project/git/Source/kwsys/Terminal.c:53:21: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]",
+            "int default_tty = color & kwsysTerminal_Color_AssumeTTY;",
+            "                    ^",
+        ]
+
+        pp._parse_lines(example_warning + example_warning)
+        diags = pp.get_diags()
+
+        self.assertEqual(len(diags), 1)
+        self.assertEqual(
+            str(diags[0]),
+            "/project/git/Source/kwsys/Terminal.c:53:21: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]"
+            "\nint default_tty = color & kwsysTerminal_Color_AssumeTTY;"
+            "\n                    ^")
+
+    def test_real_diags(self):
+        pp = ParseClangTidyDiagnostics()
+        excerpt = \
+"""/project/git/Source/kwsys/Base64.c:54:35: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  dest[0] = kwsysBase64EncodeChar((src[0] >> 2) & 0x3F);
+                                  ^
+/project/git/Source/kwsys/Base64.c:54:36: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  dest[0] = kwsysBase64EncodeChar((src[0] >> 2) & 0x3F);
+                                   ^
+/project/git/Source/kwsys/Base64.c:56:27: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+    kwsysBase64EncodeChar(((src[0] << 4) & 0x30) | ((src[1] >> 4) & 0x0F));
+                          ^
+/project/git/Source/kwsys/Base64.c:56:28: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+    kwsysBase64EncodeChar(((src[0] << 4) & 0x30) | ((src[1] >> 4) & 0x0F));
+                           ^
+/project/git/Source/kwsys/Base64.c:54:35: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  dest[0] = kwsysBase64EncodeChar((src[0] >> 2) & 0x3F);
+                                  ^
+/project/git/Source/kwsys/Base64.c:54:36: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  dest[0] = kwsysBase64EncodeChar((src[0] >> 2) & 0x3F);
+                                   ^
+/project/git/Source/kwsys/Base64.c:56:27: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+    kwsysBase64EncodeChar(((src[0] << 4) & 0x30) | ((src[1] >> 4) & 0x0F));
+                          ^
+/project/git/Source/kwsys/Base64.c:56:28: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+    kwsysBase64EncodeChar(((src[0] << 4) & 0x30) | ((src[1] >> 4) & 0x0F));
+                           ^
+/project/git/Source/kwsys/testCommandLineArguments.cxx:16:10: warning: inclusion of deprecated C++ header 'string.h'; consider using 'cstring' instead [hicpp-deprecated-headers]
+#include <string.h> /* strcmp */
+         ^~~~~~~~~~
+         <cstring>
+/project/git/Source/kwsys/testFStream.cxx:10:10: warning: inclusion of deprecated C++ header 'string.h'; consider using 'cstring' instead [hicpp-deprecated-headers]
+#include <string.h>
+         ^~~~~~~~~~
+         <cstring>
+/project/git/Source/kwsys/testFStream.cxx:77:47: warning: do not implicitly decay an array into a pointer; consider using gsl::array_view or an explicit cast instead [hicpp-no-array-decay]
+      out.write(reinterpret_cast<const char*>(expected_bom_data[i] + 1),
+                                              ^
+/project/git/Source/kwsys/testFStream.cxx:78:18: warning: do not implicitly decay an array into a pointer; consider using gsl::array_view or an explicit cast instead [hicpp-no-array-decay]
+                *expected_bom_data[i]);
+                 ^
+/project/git/Source/kwsys/testFStream.cxx:109:3: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  ret |= testNoFile();
+  ^
+/project/git/Source/kwsys/testFStream.cxx:110:3: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+  ret |= testBOM();
+  ^
+/project/git/Source/kwsys/testSystemInformation.cxx:83:36: warning: use of a signed integer operand with a binary bitwise operator [hicpp-signed-bitwise]
+    if (info.DoesCPUSupportFeature(static_cast<long int>(1) << i)) {
+                                   ^"""
+        pp.parse_string(excerpt)
+        self.assertEqual(len(pp.get_diags()), 11)
+
+        self.maxDiff = None
+        generated_diag = "\n".join(str(diag) for diag in pp.get_diags())
+        # It is not identical because of deduplication.
+        self.assertNotEqual(generated_diag, excerpt)
+
+        # The first 11 lines are duplicated diags but the rest is identical.
+        self.assertEqual(generated_diag,
+                         "\n".join(excerpt.splitlines()[12:]))
 
 
 if __name__ == "__main__":
