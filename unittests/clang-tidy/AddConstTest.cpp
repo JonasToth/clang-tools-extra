69c689a9cfd52277eaac24d1d58f33feb5d9421f
Jonas Toth <development@jonas-toth.eu>
2018-11-07 06:47:33 +0100

    [Misc] start working on unit tests

--- unittests/clang-tidy/AddConstTest.cpp
+++ unittests/clang-tidy/AddConstTest.cpp
@@ -1,2 +1,21 @@
+#include "../clang-tidy/utils/FixItHintUtils.h"
 #include "ClangTidyTest.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/Tooling.h"
 #include "gtest/gtest.h"
+
+namespace {
+std::unique_ptr<ASTUnit>
+buildASTFromCodeWithArgs(const Twine &Code,
+                         const std::vector<std::string> &Args) {
+  auto AST = tooling::buildASTFromCodeWithArgs(Code, Args);
+  EXPECT_FALSE(AST->getDiagnostics().hasErrorOccurred());
+  return AST;
+}
+
+std::unique_ptr<ASTUnit> buildASTFromCode(const Twine &Code) {
+  return buildASTFromCodeWithArgs(Code, {});
+}
+
+} // namespace
