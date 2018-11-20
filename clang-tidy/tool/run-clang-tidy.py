66e0c04cce286636f7f7b082e2b479426b86d60c
Jonas Toth <development@jonas-toth.eu>
2018-11-20 14:41:35 +0100

    [Misc] adjust encoding non throwing on errors

--- clang-tidy/tool/run-clang-tidy.py
+++ clang-tidy/tool/run-clang-tidy.py
@@ -344,9 +344,9 @@ def run_tidy(args, tmpdir, build_path, queue, lock, failed_files, parser):
     with lock:
       invoc = ' '.join(invocation) + '\n'
       if parser:
-        parser.parse_string(output.decode('utf-8'))
+        parser.parse_string(output.decode('utf-8', 'backslashreplace'))
         sys.stdout.write(invoc\
-                         + '\n'.join([str(diag).decode('utf-8') for diag in parser.get_diags()])\
+                         + '\n'.join([str(diag).encode('utf-8', 'backslashreplace') for diag in parser.get_diags()])\
                          + '\n')
         parser.reset_parser()
       else:
