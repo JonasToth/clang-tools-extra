d656f45f6912635566bcb2337d73a1fc367fc374
Jonas Toth <development@jonas-toth.eu>
2018-11-28 23:11:44 +0100

    [Fix] missing brace

--- test/clang-tidy/cppcoreguidelines-const-correctness-transform-pointer-as-values.cpp
+++ test/clang-tidy/cppcoreguidelines-const-correctness-transform-pointer-as-values.cpp
@@ -10,4 +10,4 @@ void potential_const_pointer() {
   double *p_local0 = &np_local0[1];
   // CHECK-MESSAGES: [[@LINE-1]]:3: warning: variable 'p_local0' of type 'double *' can be declared 'const'
   // CHECK-FIXES: const
-
+}
