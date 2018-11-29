205f18e52586d63162dfdd921595e803058f736e
Jonas Toth <development@jonas-toth.eu>
2018-11-29 21:36:54 +0100

    [Fix] logic bug, and/or swap

--- clang-tidy/tool/run-clang-tidy.py
+++ clang-tidy/tool/run-clang-tidy.py
@@ -356,7 +356,7 @@ def run_tidy(args, tmpdir, build_path, queue, lock, failed_files, parser):
 
       if len(err) > 0:
         err_lines = err.splitlines()
-        errors = [l for l in err_lines if ("warnings generated" not in l) or ("warning generated" not in l)]
+        errors = [l for l in err_lines if ("warnings generated" not in l) and ("warning generated" not in l)]
         for l in errors:
             sys.stderr.write((l + "\n").decode('utf-8', 'backslashreplace'))
       sys.stderr.flush()
