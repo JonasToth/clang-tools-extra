139227091dbc00f3901f3916b20a6da2cab84729
Jonas Toth <development@jonas-toth.eu>
2019-01-23 19:58:41 +0100

    add license to exceptionanalyzer

--- clang-tidy/utils/ExceptionAnalyzer.cpp
+++ clang-tidy/utils/ExceptionAnalyzer.cpp
@@ -1,3 +1,11 @@
+//===--- ExceptionAnalyzer.cpp - clang-tidy -------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "ExceptionAnalyzer.h"
 
 #include "clang/AST/ASTContext.h"
